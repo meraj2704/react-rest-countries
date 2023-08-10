8504ac9e87c899ccb2cccda386098673e04c4752
IJE2704 <mh1669101@gmail.com>
2023-08-10 20:59:30 +0600

    load countries using useeffect

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ function LoadCountries()
 {
   const [countries, setCountries] = useState([]);
   useEffect(()=>{
-    
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=> res.json())
+    .then(data => setCountries(data))
   },[])
   return (
     <div>
       <h1>Visiting Every country of the world!!</h1>
+      <h4>Country Length : {countries.length}</h4>
     </div>
   )
 }
