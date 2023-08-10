ade53c7516d4c091c4a3b7ca0debff3bad7bb082
IJE2704 <mh1669101@gmail.com>
2023-08-10 21:59:03 +0600

    display countries in simple way using api

--- src/App.js
+++ src/App.js
@@ -18,10 +18,28 @@ function LoadCountries()
     .then(res=> res.json())
     .then(data => setCountries(data))
   },[])
+  console.log(countries)
   return (
     <div>
       <h1>Visiting Every country of the world!!</h1>
       <h4>Country Length : {countries.length}</h4>
+      
+      {
+        countries.map(country => <Country name={country.name.common} capital={country.capital} population={country.population} region={country.region} area={country.area} flag={country.flags.png}></Country>)
+      }
+    </div>
+  )
+}
+function Country(props)
+{
+  return(
+    <div className='country'>
+      <h1>Name : {props.name}</h1>
+      <p>Capital : {props.capital}</p>
+      <p>Area : {props.area}</p>
+      <p>Region : {props.region}</p>
+      <p>Population : {props.population}</p>
+      <img src={props.flag} alt="" />
     </div>
   )
 }
